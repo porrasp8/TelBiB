55a9c23efae70778c5aab2dea9e398842a10982e
porrasp8 <ivanporras44@gmail.com>
2024-02-14 14:46:43 +0100

    Rename

---
+++ UR3_VrSrc/RobotCamera.gd
@@ -0,0 +1,70 @@
+extends Node3D
+
+#-- Global vars
+var http_request : HTTPRequest
+
+#-- Constants
+const  CAMARA_URL = "http://192.168.0.193:8080/shot.jpg"
+const  IMAGE_DIR = "res://captured_images/"
+const  REQUEST_INTERVAL = 0.01
+
+#-- Load urbot node to allow communication
+var urbot_script = load("res://urbot.cs")
+var urbot_node = urbot_script.new()
+
+func _ready():
+	
+	#-- HTTP request init
+	http_request = HTTPRequest.new()
+	add_child(http_request)
+	
+	#-- Init callback and connect
+	http_request.request_completed.connect(self._http_request_completed)
+	
+	_make_request()
+
+
+func _process(delta):
+
+	#-- New request when previous finished
+	print("HTTP status: ", http_request.get_http_client_status())
+	if (http_request.get_http_client_status() != HTTPClient.STATUS_REQUESTING and 
+		http_request.get_http_client_status() != HTTPClient.STATUS_CONNECTING and
+		http_request.get_http_client_status() != HTTPClient.STATUS_BODY and 
+		http_request.get_http_client_status() != HTTPClient.STATUS_CONNECTED ) : 
+		_make_request()
+
+
+func _make_request():
+
+	#-- Make a new http request and check error
+	var http_error = http_request.request(CAMARA_URL)
+	if http_error != OK:
+		print("An error occurred in the HTTP request.")
+
+func _http_request_completed(result, response_code, headers, body):
+
+	if response_code == 200:
+		#-- Load new image
+		var image = Image.new()
+		var image_error = image.load_jpg_from_buffer(body)
+		if image_error != OK:
+			print("An error occurred while trying to display the image.")
+		
+		#-- Transform image to texture and assign to the 3D
+		$RobotCameraWindow.texture = ImageTexture.create_from_image(image)
+		print("HTTP: texture changed")
+		
+		#-- Save image(Debugging)
+		'''
+		var image_filename = "captured_image.jpg"
+		var image_path = IMAGE_DIR + image_filename
+		image.save_jpg(image_path)
+		print("Image saved:", image_path)
+		
+		var image2 = Image.load_from_file(image_path)
+		'''
+		
+	else:
+		print("Error in HTTP request. Response code:", response_code)
+
